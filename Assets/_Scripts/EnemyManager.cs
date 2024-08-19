7d2bf40ff65af14952457d678232c9037fd2133d
jjustinjc <caijjustin@gmail.com>
2024-08-18 20:13:35 -0400

    enemyWIP, enemymanagerWIP, turnmanagerWIP

---
+++ Assets/_Scripts/EnemyManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager Instance {get;private set ;}
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+        if (Instance != null)
+        {
+            Debug.LogError("more than one instance bruh");
+        }
+
+        Instance = this;
+    }
+    
+    private void SearchSpawnVector(){
+        Vector3 origin = Player.Instance.transform.position;
+        Vector3 playerScale = Player.Instance.PlayerScale
+        origin += new Vector3(0, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/TurnManager.cs
@@ -0,0 +1,27 @@
+    
+    using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public enum TurnSequence{START, CYCLE, PACTIONS, EACTIONS, END}
+
+public class TurnManager : MonoBehaviour
+{
+    [SerializeField] private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void GameSetup(){
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
