0a3a95dc60d60734979b8196de0723063e6a127d
jjustinjc <caijjustin@gmail.com>
2024-08-20 05:07:24 -0400

    enemyshootcooldowns

--- Assets/_Scripts/EnemyShoot.cs
+++ Assets/_Scripts/EnemyShoot.cs
@@ -8,11 +8,21 @@ public class EnemyShoot : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     private Quaternion aimDir;
-    [SerializeField] private int Cooldown;
+    [SerializeField] private int cooldown;
+    private int count;
 
     [SerializeField] private Collider enemyCollider;
 
     // Update is called once per frame
+    void Awake()
+    {
+        TurnManager.Instance.OnTurnCountChanged += Instance_OnTurnCountChanged;
+    }
+    private void Instance_OnTurnCountChanged(object sender, TurnManager.OnTurnCountChangedEventArgs e)
+    {
+        count++;
+    }
+    
     void Update()
     {
 
@@ -24,12 +34,15 @@ public class EnemyShoot : MonoBehaviour
     }
     public void Shoot()
     {
+        if(count>=cooldown){
+            count = 0;
         GameObject bullet = Instantiate(bulletPrefab, transform.position, aimDir);
         // Instantiate(bulletPrefab, transform.position, Quaternion.identity).transform.rotation.SetLookRotation(toPlayer);
 
         bullet.transform.rotation = aimDir;
 
         Physics.IgnoreCollision(bullet.GetComponent<Collider>(), enemyCollider);
+        }
 
     }
 }
