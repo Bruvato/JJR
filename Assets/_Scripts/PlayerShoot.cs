4051ac1f08244ae0ed80455225b643da651dfe66
Bruvato <ray01778@gmail.com>
2024-08-20 06:47:19 -0400

    o

--- Assets/_Scripts/PlayerShoot.cs
+++ Assets/_Scripts/PlayerShoot.cs
@@ -9,16 +9,45 @@ public class PlayerShoot : MonoBehaviour
 
     [SerializeField] private Collider playerCollider;
 
+    private LineRenderer lineRenderer;
+    [SerializeField] private float maxAimRange = 50f;
+
+    private bool lockedIn;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+    }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            lockedIn = true;
         }
+
+        RaycastHit hit;
+        Vector3 endPoint;
+
+        if (Physics.Raycast(firePoint.position + firePoint.forward * Player.Instance.GetMaxScaleComponent(), firePoint.forward, out hit, maxAimRange))
+        {
+            lineRenderer.enabled = true;
+            endPoint = hit.point;
+        }
+        
+
+        endPoint = firePoint.position + firePoint.forward * maxAimRange;
+
+
+        lineRenderer.SetPosition(1, endPoint);
+
+        
     }
     void Shoot()
     {
+        lockedIn = false;
+
         GameObject bullet = Instantiate(projectilePrefab, Player.Instance.transform.position, firePoint.rotation);
 
         Physics.IgnoreCollision(bullet.GetComponent<Collider>(), playerCollider);
