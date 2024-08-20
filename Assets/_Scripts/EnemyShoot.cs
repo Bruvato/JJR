a0aa82d80b3cc7aa25b51b62f87922fd3ba9679d
jjustinjc <caijjustin@gmail.com>
2024-08-20 05:56:37 -0400

    displays aim when enemy is about to shoot

--- Assets/_Scripts/EnemyShoot.cs
+++ Assets/_Scripts/EnemyShoot.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyShoot : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     private Quaternion aimDir;
     [SerializeField] private int cooldown;
+    [SerializeField] private LineRenderer lineRenderer;
     private int count;
-
+    
     [SerializeField] private Collider enemyCollider;
 
     // Update is called once per frame
@@ -30,12 +32,22 @@ public class EnemyShoot : MonoBehaviour
     public void Aim()
     {
         aimDir = Quaternion.LookRotation(Player.Instance.transform.position - transform.position).normalized;
+        if(count>=cooldown){
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position+Vector3.Scale((Player.Instance.transform.position - transform.position).normalized, new Vector3(2,2,2)));
+
+        }else{
+            lineRenderer.positionCount= 0;
+        }
 
     }
     public void Shoot()
     {
         if(count>=cooldown){
-            count = 0;
+
+        count = 0;
+        
         GameObject bullet = Instantiate(bulletPrefab, transform.position, aimDir);
         // Instantiate(bulletPrefab, transform.position, Quaternion.identity).transform.rotation.SetLookRotation(toPlayer);
 
